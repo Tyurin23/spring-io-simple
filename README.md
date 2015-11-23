81d92fa724300a63974cfdc9caaa537e0e612f46
Y. Tyurin <tyurin23@gmail.com>
2015-11-23 12:28:17 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Install
+```
+mvn install
+```
+
+# Using
+```
+mvn archetype:generate \
+  -DarchetypeGroupId=ru.tyurin23 \
+  -DarchetypeArtifactId=spring-io-simple \
+  -DarchetypeVersion=1.0 \
+  -DgroupId=your-groupId \
+  -DartifactId=your-artifactId
+```
---
+++ src/main/resources/archetype-resources/src/test/java/TestInitializer.java
@@ -0,0 +1,21 @@
+package $package;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@ActiveProfiles(profiles = {"it", "test"})
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+@Ignore
+public class TestInitializer {
+}
